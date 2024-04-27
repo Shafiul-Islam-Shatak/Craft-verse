283930796b88c60fff93a29c68bbb6b95d10e5b9
Shatak-the-imposter <helloshatak@gmail.com>
2024-04-28 00:29:44 +0600

    post method created and sweet art implemented

--- src/Pages/AddCraft.jsx
+++ src/Pages/AddCraft.jsx
@@ -1,4 +1,7 @@
 import { Helmet } from "react-helmet-async";
+import Swal from 'sweetalert2'
+// import 'sweetalert2/src/sweetalert2.scss'
+
 
 const AddCraft = () => {
     const hanldeAddCraft = e => {
@@ -15,8 +18,30 @@ const AddCraft = () => {
         const userEmail = form.email.value;
         const customaization = form.customaization.value;
         const description = form.description.value;
-        const craft = { item_name, sub_catagory, image, price, rating, time, customaization, stock , userName,userEmail, description };
+        const craft = { item_name, sub_catagory, image, price, rating, time, customaization, stock, userName, userEmail, description };
         console.log(craft);
+
+        // post data to backend
+        fetch('http://localhost:7000/craft', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(craft)
+
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.insertedId) {
+                    Swal.fire({
+                        title: "Congratulation !!",
+                        text: "Succesfully Craft Added",
+                        icon: "success"
+                    });
+                    form.reset();
+                }
+            })
     }
     return (
         <div>
@@ -25,21 +50,21 @@ const AddCraft = () => {
                     Craft Verse-Add Craft
                 </title>
             </Helmet>
-            <div >
+            <div className="bg[#F4F3F0]" >
                 <div className="hero-overlay bg-opacity-60"></div>
                 <h2 className="text-center text-2xl md:text-3xl lg:text-5xl font-bold mb-5">Add Your Craft Here</h2>
                 <div className="w-3/4 mx-auto">
                     <form onSubmit={hanldeAddCraft}>
-                        <div className='space-y-3 md:space-y-5'>
+                        <div className='space-y-3 lg:space-y-5'>
 
-                            <div className="flex justify-between space-x-5">
+                            <div className="lg:flex lg:justify-between lg:space-x-5 space-y-3 lg:space-y-0">
                                 <label className="input input-bordered grow flex items-center gap-2">
-                                    Your Craft Name :
-                                    <input type="text" name="item_name" className="" placeholder="Your Craft Name Here" />
+                                    Craft Name :
+                                    <input type="text" name="item_name" className="grow" placeholder="Your Craft Name Here" />
                                 </label>
                                 <label className="input input-bordered grow flex items-center gap-2">
                                     Sub-Catagory :
-                                    <select name="catagory">
+                                    <select name="catagory" className="grow">
                                         <option value="">Not Selected</option>
                                         <option value="Landscape Painting">Landscape Painting</option>
                                         <option value="Portrait Drawing">Portrait Drawing</option>
@@ -51,18 +76,18 @@ const AddCraft = () => {
                                 </label>
                             </div>
 
-                            <div className="flex justify-between space-x-5">
+                            <div className="lg:flex lg:justify-between lg:space-x-5 space-y-3 lg:space-y-0">
                                 <label className="input input-bordered flex grow items-center gap-2">
                                     Image :
-                                    <input type="url" name="photoURL" className="" placeholder="https://" />
+                                    <input type="url" name="photoURL" className="grow" placeholder="https://" />
                                 </label>
                                 <label className="input input-bordered grow flex items-center gap-2">
                                     Price :
-                                    <input type="text" name="price" className=" " placeholder="4000" />
+                                    <input type="text" name="price" className="grow " placeholder="4000" />
                                 </label>
 
                             </div>
-                            <div className="flex justify-between space-x-5">
+                            <div className="lg:flex lg:justify-between lg:space-x-5 space-y-3 lg:space-y-0">
 
                                 <label className="input input-bordered flex grow items-center gap-2">
                                     Rating :
@@ -73,10 +98,10 @@ const AddCraft = () => {
                                     <input type="text" name="time" className="grow" placeholder="In weeks" />
                                 </label>
                             </div>
-                            <div className="flex justify-between space-x-5">
+                            <div className="lg:flex lg:justify-between lg:space-x-5 space-y-3 lg:space-y-0 ">
                                 <label className="input input-bordered flex grow items-center gap-2">
                                     Customization :
-                                    <select name="customaization"  >
+                                    <select name="customaization" className="grow">
                                         <option value="">Not Selected</option>
                                         <option value="Yes">Yes</option>
                                         <option value="No">No</option>
@@ -84,7 +109,7 @@ const AddCraft = () => {
                                 </label>
                                 <label className="input input-bordered flex grow items-center gap-2">
                                     Stock Status :
-                                    <select name="stock"  >
+                                    <select name="stock" className="grow" >
                                         <option value="">Not Selected</option>
                                         <option value="Yes">In Stock</option>
                                         <option value="No">Made in Order</option>
@@ -92,19 +117,19 @@ const AddCraft = () => {
                                 </label>
 
                             </div>
-                            <div className="flex justify-between space-x-5">
+                            <div className="lg:flex lg:justify-between lg:space-x-5 space-y-3 lg:space-y-0">
                                 <label className="input input-bordered flex grow items-center gap-2">
                                     User Name :
-                                    <input type="text" name="user_name" className="" placeholder="Your Name Here" />
+                                    <input type="text" name="user_name" className="grow" placeholder="Your Name Here" />
                                 </label>
                                 <label className="input input-bordered flex grow items-center gap-2">
                                     User Email :
-                                    <input type="email" name="email" className="" placeholder="Your Email Here" />
+                                    <input type="email" name="email" className="grow" placeholder="Your Email Here" />
                                 </label>
                             </div>
                             <label className="input input-bordered flex grow items-center gap-2">
-                                Short Description :
-                                <input type="text" name="description" className="" placeholder="About Your Craft" />
+                                Description :
+                                <input type="text" name="description" className="grow" placeholder="Write a short description" />
                             </label>
 
                         </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,14 @@
+import { Helmet } from "react-helmet-async";
 import Banner from "../Components/Banner";
 
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title>
+                    Craft Verse - Home
+                </title>
+            </Helmet>
             <Banner></Banner>
          
         </div>
